95508190c566b89cff078a6128d258f4903a797e
Sasha Shkoda <shkoda_sashka@mail.ru>
2025-10-19 23:20:24 +0300

    Попытка понять почему не работает input

---
+++ tests/test_password_out_of_range_in_registration copy.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from helper import generate_registration_data
+from locators import Locators
+from curl import url
+
+class TestRegistrationWithPasswordOutOfRange:
+
+    def test_unsuccess_registration_with_invalid_password(self, driver):
+        #arrange
+        driver.get(url.registration_site)
+        name, email = generate_registration_data()
+        driver.find_element(*Locators.NAME).send_keys(name)       
+        driver.find_element(*Locators.EMAIL).send_keys(email)
+        driver.find_element(*Locators.PASSWORD).send_keys('12345')
+
+        #act
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
+        popup_text=WebDriverWait(driver, 10).until(EC.visibility_of_element_located(*Locators.SIGN_BUTTON)).text
+        #assert
+        assert popup_text == 'Некорректный пароль'
