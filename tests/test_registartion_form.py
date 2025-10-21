c16dbd36746150734f9811a2841565d46352d0e7
Sasha Shkoda <shkoda_sashka@mail.ru>
2025-10-21 20:12:26 +0300

    Исправлено замечание по ожиданию появления элемента на странцие, а не его текста

--- tests/test_registartion_form.py
+++ tests/test_registartion_form.py
@@ -1,37 +1,28 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from helper import generate_registration_data
 from locators import Locators
 from curl import url
 
+
 class TestRegistrationForm:
 
     def test_success_registration(self, driver):
-        #arrange
         driver.get(url.registration_site)
         name, email, password = generate_registration_data()
         driver.find_element(*Locators.NAME_REGISTER).send_keys(name)       
         driver.find_element(*Locators.EMAIL_REGISTER).send_keys(email)
         driver.find_element(*Locators.PASSWORD_REGISTER).send_keys(password)
-
-        #act
         driver.find_element(*Locators.REGISTER_BUTTON).click()
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.SIGN_BUTTON))
-        #assert
         assert driver.current_url == url.login_site
 
     def test_unsuccess_registration_with_invalid_password(self, driver):
-        #arrange
         driver.get(url.registration_site)
         name, email, password = generate_registration_data()
         driver.find_element(*Locators.NAME_REGISTER).send_keys(name)       
         driver.find_element(*Locators.EMAIL_REGISTER).send_keys(email)
         driver.find_element(*Locators.PASSWORD_REGISTER).send_keys(password[:5])
-
-        #act
         driver.find_element(*Locators.REGISTER_BUTTON).click()
-        popup_text = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.INVALID_PASSWORD_POPUP)).text
-        #assert
-        assert popup_text == 'Некорректный пароль'
+        assert WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.INVALID_PASSWORD_POPUP))
 
